f69b92e711258dd43ca01c11b5db7a664ff46ab0
Guntler <pbpdirector@hotmail.com>
2017-01-22 12:43:54 +0000

    Title

---
+++ Assets/IntroBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroBehavior : MonoBehaviour {
+    public Light EggSpotlight;
+    public Light EggDirectional;
+    public Image SplashScreen;
+    public Canvas Canvas;
+
+    public AudioClip IntroVoice;
+
+    AudioSource src;
+
+    bool showedSplashScreenP1 = false;
+    bool showedSplashScreen = false;
+    float elapsedWaitSplash = 0;
+    float waitSplashScreen = 2f;
+
+    // Use this for initialization
+    void Start () {
+        src = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!showedSplashScreen)
+        {
+            if (showedSplashScreenP1)
+            {
+                elapsedWaitSplash += Time.deltaTime;
+                if(elapsedWaitSplash > waitSplashScreen)
+                {
+                    SplashScreen.color = new Color(1, 1, 1, SplashScreen.color.a - Time.deltaTime * 0.5f);
+                    if (SplashScreen.color.a <= 0.0f)
+                        showedSplashScreen = true;
+                }
+            }
+            else
+            {
+                SplashScreen.color = new Color(1,1,1,SplashScreen.color.a + Time.deltaTime*0.5f);
+                if (SplashScreen.color.a >= 1.0f)
+                    showedSplashScreenP1 = true;
+            }
+        }
+        else
+        {
+            elapsedWaitSplash = 0;
+            if (EggSpotlight.intensity < 8)
+            {
+                EggSpotlight.intensity += Time.deltaTime * 2.0f;
+            }
+            else
+            {
+                if (EggDirectional.intensity < 4)
+                {
+                    EggDirectional.intensity += Time.deltaTime * 10.0f;
+                    if (EggDirectional.intensity >= 3 && EggDirectional.intensity < 4)
+                    {
+                        src.PlayOneShot(IntroVoice);
+                        Canvas.gameObject.SetActive(true);
+
+                    }
+                }
+            }
+        }
+	}
+
+    IEnumerator DoIntroSequence()
+    {
+
+        yield return null;
+    }
+}
---
+++ Assets/TitleEggBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleEggBehavior : MonoBehaviour {
+
+    int _multiplier = 1;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0,1,0), 10.0f*Time.deltaTime);
+        if(transform.position.y >= 1.75f)
+        {
+            _multiplier = -1;
+        }
+        else if (transform.position.y <= 1.25f)
+        {
+            _multiplier = 1;
+        }
+        transform.position = Vector3.LerpUnclamped(transform.position, transform.position + new Vector3(0, Time.deltaTime * 2.0f * _multiplier, 0),Time.deltaTime*2.0f);
+	}
+}
